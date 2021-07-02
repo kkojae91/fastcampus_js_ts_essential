e5e5d30c50bc6e8bb0a559296ab5ddc5ef36a067
kkojae <jaejeung3210@gmail.com>
2021-07-02 23:06:12 +0900

    ts 맛보기

---
+++ test.ts
@@ -0,0 +1,10 @@
+type Centimeter = number;
+type RainbowColor = 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'indigo' |'violet';
+
+let age = 10;
+let weight : number = 80;
+let height : Centimeter = 176;
+let colot : RainbowColor = 'orange';
+
+// 타입에 맞지 않을 경우 Error가 발생한다.
+color = 'black';
