6e4b0d9e55625eb703834bcde9593034bed47142
kkojae <jaejeung3210@gmail.com>
2021-07-04 19:57:27 +0900

    hackerNewsClient 복잡한 UI 구현을 위한 준비 작업 - 탬플릿

--- hackerNewsWithJS/app.js
+++ hackerNewsWithJS/app.js
@@ -21,8 +21,18 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsFeedLength = newsFeed.length / 10;
   const newsList = [];
-
-  newsList.push("<ul>");
+  let template = `
+    <div class="container mx-auto p-4">
+      <h1>Hacker News</h1>
+      <ul>
+        {{__news_feed__}}
+      </ul>
+      <div>
+        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+      </div>
+    </div>
+  `;
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
@@ -31,14 +41,12 @@ function newsFeed() {
     </li>
     `);
   }
-  newsList.push("</ul>");
-  newsList.push(`
-    <div>
-      <a href='#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}'>이전 페이지</a>
-      <a href='#/page/${store.currentPage < newsFeedLength ? store.currentPage + 1 : 3}'>다음 페이지</a>
-    </div>
-  `);
-  container.innerHTML = newsList.join("");
+
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace("{{__prev_page__}}", store.currentPage > 1 ? store.currentPage - 1 : 1);
+  template = template.replace("{{__next_page__}}", store.currentPage < newsFeedLength ? store.currentPage + 1 : 3);
+
+  container.innerHTML = template;
 }
 
 function newsDetail() {
