d07a455c6ffff22dedb386e074e68a02e4c45f82
kkojae <jaejeung3210@gmail.com>
2021-07-04 15:59:19 +0900

    hackerNewsClient 문자열을 활용한 HTML 다루기

--- hackerNewsWithJS/app.js
+++ hackerNewsWithJS/app.js
@@ -5,37 +5,42 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-// 동기적으로 처리할 경우 마지막 옵션 값을 false를 주면 됩니다.
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
+function getData(url) {
+  // 동기적으로 처리할 경우 마지막 옵션 값을 false를 주면 됩니다.
+  ajax.open("GET", url, false);
+  ajax.send();
 
-// ajax.response 를 사용하면 값을 불러 올 수 있다 JSON 타입
-// JSON파일을 객체화 시켜주기 위해 JSON.parse를 사용!
-const newsFeed = JSON.parse(ajax.response);
+  // ajax.response 를 사용하면 값을 불러 올 수 있다 JSON 타입
+  // JSON파일을 객체화 시켜주기 위해 JSON.parse를 사용!
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
 window.addEventListener("hashchange", () => {
   // substr(startIndex, length), substring(startIndex, endIndex), slice(startIndex, endIndex) 문자열 잘라내는 방법!
   const id = location.hash.substr(1);
 
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
   const title = document.createElement("h1");
+
   title.innerHTML = newsContent.title;
   content.appendChild(title);
-  // console.log(newsContent);
 });
 
+// dom API를 최대한 사용하지 않고 innerHTML을 사용해서 마크업 구조를 한눈에 보기 쉽게 수정
 for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  const div = document.createElement("div");
 
-  li.append(a);
-  ul.appendChild(li);
+  div.innerHTML = `
+  <li>
+    <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+  </li>
+  `;
+
+  // div는 담아주는 그릇이였기때문에, ul테그 안에 firstElementChild를 활용해서 li태그만 append 해준다.
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
--- hackerNewsWithJS/dist/app.c328ef1a.js
+++ hackerNewsWithJS/dist/app.c328ef1a.js
@@ -124,32 +124,33 @@ var container = document.querySelector("#root");
 var ajax = new XMLHttpRequest();
 var content = document.createElement("div");
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
-var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json"; // 동기적으로 처리할 경우 마지막 옵션 값을 false를 주면 됩니다.
+var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEWS_URL, false);
-ajax.send(); // ajax.response 를 사용하면 값을 불러 올 수 있다 JSON 타입
-// JSON파일을 객체화 시켜주기 위해 JSON.parse를 사용!
+function getData(url) {
+  // 동기적으로 처리할 경우 마지막 옵션 값을 false를 주면 됩니다.
+  ajax.open("GET", url, false);
+  ajax.send(); // ajax.response 를 사용하면 값을 불러 올 수 있다 JSON 타입
+  // JSON파일을 객체화 시켜주기 위해 JSON.parse를 사용!
 
-var newsFeed = JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
+}
+
+var newsFeed = getData(NEWS_URL);
 var ul = document.createElement("ul");
 window.addEventListener("hashchange", function () {
   // substr(startIndex, length), substring(startIndex, endIndex), slice(startIndex, endIndex) 문자열 잘라내는 방법!
   var id = location.hash.substr(1);
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-  var newsContent = JSON.parse(ajax.response);
+  var newsContent = getData(CONTENT_URL.replace("@id", id));
   var title = document.createElement("h1");
   title.innerHTML = newsContent.title;
-  content.appendChild(title); // console.log(newsContent);
-});
+  content.appendChild(title);
+}); // dom API를 최대한 사용하지 않고 innerHTML을 사용해서 마크업 구조를 한눈에 보기 쉽게 수정
 
 for (var i = 0; i < 10; i++) {
-  var li = document.createElement("li");
-  var a = document.createElement("a");
-  a.href = "#".concat(newsFeed[i].id);
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
-  li.append(a);
-  ul.appendChild(li);
+  var div = document.createElement("div");
+  div.innerHTML = "\n  <li>\n    <a href=\"#".concat(newsFeed[i].id, "\">").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")</a>\n  </li>\n  "); // div는 담아주는 그릇이였기때문에, ul테그 안에 firstElementChild를 활용해서 li태그만 append 해준다.
+
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
